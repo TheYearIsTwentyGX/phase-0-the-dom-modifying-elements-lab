bd07ef11c9c8135d3180af0a048ce605ecbbb16d
Dylan Hays <twentygx@outlook.com>
2022-07-22 18:26:00 -0500

    commit tree 6adef30f0077219fdf6bef6dde5caeaacda4d90c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
+document.getElementsByTagName('body')[0].appendChild(newHeader);
